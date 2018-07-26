019b06d71ee76db5aa7fc5ec5ee4221bb265d6c7
chaiwat <chaiwat.si@jorakay.com>
2018-07-26 11:28:00 +0700

    modified module asset _form date input

--- backend/modules/assets/views/assets/_form.php
+++ backend/modules/assets/views/assets/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Assets */
@@ -10,9 +11,11 @@ use yii\widgets\ActiveForm;
 
 <div class="assets-form">
 
-    <?php $form = ActiveForm::begin([
-        'options' => ['enctype' => 'multipart/form-data']
-    ]); ?>
+    <?php
+    $form = ActiveForm::begin([
+                'options' => ['enctype' => 'multipart/form-data']
+    ]);
+    ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
@@ -22,7 +25,18 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'mac_address')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'purchase_date')->textInput() ?>
+    <?php
+    echo '<label>วันที่ซื้อ</label>';
+    echo DatePicker::widget([
+        'name' => 'purchase_date',
+        'value' => date('yyyy-dd-mm', strtotime('+2 days')),
+        'options' => ['placeholder' => 'Select issue date ...'],
+        'pluginOptions' => [
+            'format' => 'yyyy-dd-mm',
+            'todayHighlight' => true
+        ]
+    ]);
+    ?>
 
     <?= $form->field($model, 'purchase_cost')->textInput(['maxlength' => true]) ?>
 
@@ -34,16 +48,16 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
-    <?= $form->field($model, 'status')->dropDownList([ 'Ready To Deploy' => 'Ready To Deploy', 'Repairing' => 'Repairing', 'Has Used' => 'Has Used', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'status')->dropDownList(['พร้อมใช้' => 'พร้อมใช้', 'ส่งซ่อม' => 'ส่งซ่อม', 'มีผู้ใช้แล้ว' => 'มีผู้ใช้แล้ว',], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'warranty_months')->textInput() ?>
+        <?= $form->field($model, 'warranty_months')->textInput() ?>
 
-    <?= $form->field($model, 'supplier_id')->textInput() ?>
+<?= $form->field($model, 'supplier_id')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+<?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
