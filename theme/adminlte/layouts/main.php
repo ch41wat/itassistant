eb3004669167a42027fc347385f6d1bd69d7d56e
chaiwat <chaiwat.si@jorakay.com>
2018-09-03 08:21:48 +0700

    modified theme

--- theme/adminlte/layouts/main.php
+++ theme/adminlte/layouts/main.php
@@ -18,6 +18,7 @@ if (Yii::$app->controller->action->id === 'login') {
 
     if (class_exists('backend\assets\AppAsset')) {
         backend\assets\AppAsset::register($this);
+        frontend\assets\AppAsset::register($this);
     } else {
         app\assets\AppAsset::register($this);
     }
